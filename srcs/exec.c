f74bcc3d63a70f00e401b646c93cdae27d11bf68
Lucas Dominique <lucasdominique99@gmail.com>
2022-05-02 22:18:08 +0200

    WIP: implemented the use of builtins with args

--- srcs/exec.c
+++ srcs/exec.c
@@ -3,19 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melogr@phy <melogr@phy.to>                 +#+  +:+       +#+        */
+/*   By: ldominiq <ldominiq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 23:55:29 by melogr@phy        #+#    #+#             */
-/*   Updated: 2022/04/13 10:23:31 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/05/02 22:17:05 by ldominiq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"minishell.h"
 
+// Works but need to press enter to see 'it's the Prompt $ :/'
 void	exec_cmd(t_cmd *cmd1)
 {
+	int	pid;
 	if (access(cmd1->cmd, X_OK) == 0)
-		printf("%s\n", cmd1->cmd);
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			execv(cmd1->cmd, cmd1->arg);
+			exit(0);
+		}
+	}
 	else
 		errmsg("minishell: command not found: ", cmd1->cmd);
 }
