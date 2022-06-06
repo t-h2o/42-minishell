faf96a8d4e0625c3e3a9056709ef5bfb5a88d87a
To grivel <grivel.theo@protonmail.com>
2022-06-06 13:54:06 +0200

    INFILE: Changed return value of str_cmp

--- srcs/build.c
+++ srcs/build.c
@@ -6,14 +6,14 @@
 /*   By: ldominiq <ldominiq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 15:30:02 by theo              #+#    #+#             */
-/*   Updated: 2022/06/03 15:17:10 by tgrivel          ###   ########.fr       */
+/*   Updated: 2022/06/05 15:48:57 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"minishell.h"
 
-//	if the two string are equal			--> return 0
-//	if the two string are not equal		--> return 1
+//	if the two string are equal			--> return 1
+//	if the two string are not equal		--> return 0
 //	if one of the two is null pointe	--> return -1
 int	str_cmp(char *s1, char *s2)
 {
@@ -25,8 +25,8 @@ int	str_cmp(char *s1, char *s2)
 		s2++;
 	}
 	if (*s1 == 0 && *s2 == 0)
-		return (0);
-	return (1);
+		return (1);
+	return (0);
 }
 
 //	if line is a build command --> return 0
@@ -35,21 +35,21 @@ int	check_build(t_cmd *cmd1, char **envp)
 {
 	if (cmd1->arg == 0)
 		return (0);
-	if (!str_cmp(cmd1->arg[0], "exit"))
+	if (str_cmp(cmd1->arg[0], "exit"))
 	{
 		return(2);
 	}
-	if (!str_cmp(cmd1->arg[0], "pwd"))
+	if (str_cmp(cmd1->arg[0], "pwd"))
 	{
 		pwd();
 		return (0);
 	}
-	if (!str_cmp(cmd1->arg[0], "env"))
+	if (str_cmp(cmd1->arg[0], "env"))
 	{
 		env(envp);
 		return (0);
 	}
-	if (!str_cmp(cmd1->arg[0], "echo"))
+	if (str_cmp(cmd1->arg[0], "echo"))
 	{
 		echo(cmd1);
 		return (0);
