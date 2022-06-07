eb017202525430cafe7b9598094251b61d33a03d
Lucas Dominique <lucasdominique99@gmail.com>
2022-06-08 00:06:29 +0200

    Fixed segfault when using echo

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,7 +6,7 @@
 /*   By: ldominiq <ldominiq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:19:06 by ldominiq          #+#    #+#             */
-/*   Updated: 2022/04/14 01:44:57 by ldominiq         ###   ########.fr       */
+/*   Updated: 2022/06/07 16:04:02 by ldominiq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,27 @@ void	echo(t_cmd *cmd1)
 	int		i;
 
 	i = 0;
-	if (!str_cmp(cmd1->arg[++i], "-n") == 0)
+	if (cmd1->arg[1] != NULL)
 	{
-		while (cmd1->arg[++i])
+		if (str_cmp(cmd1->arg[++i], "-n") != 0)
 		{
-			printf("%s", cmd1->arg[i]);
-			if (cmd1->arg[i + 1])
-				printf(" ");
+			while (cmd1->arg[++i])
+			{
+				printf("%s", cmd1->arg[i]);
+				if (cmd1->arg[i + 1])
+					printf(" ");
+			}
 		}
-	}
-	else
-	{
-		i--;
-		while (cmd1->arg[++i])
+		else
 		{
-			printf("%s", cmd1->arg[i]);
-			if (cmd1->arg[i + 1])
-				printf(" ");
+			i--;
+			while (cmd1->arg[++i])
+			{
+				printf("%s", cmd1->arg[i]);
+				if (cmd1->arg[i + 1])
+					printf(" ");
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 }
