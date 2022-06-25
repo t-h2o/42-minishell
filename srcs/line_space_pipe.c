4a9f4a15075550791f2b7af51735611a2faa0325
Leo Gyger <lgyger@student.42lausanne.ch>
2022-06-25 16:56:52 +0200

    norminatted it

--- srcs/line_space_pipe.c
+++ srcs/line_space_pipe.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   line_space_pipe.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgyger <marvin@42lausanne.ch>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/25 16:52:12 by lgyger            #+#    #+#             */
+/*   Updated: 2022/06/25 16:55:38 by lgyger           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 #include <string.h>
 
 char	*trim_space(char *line)
 {
-	int	i;
-	int	pos;
+	int		i;
+	int		pos;
 	char	*str;
 
 	pos = strchr(line, '|') - line;
 	if (pos < 0)
-		return line;
+		return (line);
 	str = malloc(strlen(line) + 3);
-	for (i = 0; i != pos; ++i)
+	i = -1;
+	while (++i != pos)
 		str[i] = line[i];
 	str[i++] = ' ';
 	str[i++] = '|';
 	str[i] = ' ';
-	++pos;
-	for ( ; line[pos]; ++pos)
+	while (line[++pos])
 		str[++i] = line[pos];
 	str[i + 1] = 0;
 	line = str;
