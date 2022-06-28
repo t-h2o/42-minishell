abec434b3b942f479ef4b944924f55a59fab564b
To grivel <grivel.theo@protonmail.com>
2022-06-28 18:18:43 +0200

    Added ()

--- srcs/utils_export.c
+++ srcs/utils_export.c
@@ -6,7 +6,7 @@
 /*   By: ldominiq <ldominiq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 14:43:00 by ldominiq          #+#    #+#             */
-/*   Updated: 2022/06/25 15:06:54 by ldominiq         ###   ########.fr       */
+/*   Updated: 2022/06/28 18:16:37 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 int	dup_table(char ***new_env, int *i, int pos, char **envp)
 {
-	*new_env = malloc((*i + 1) * sizeof(char *));
-	if (*new_env == 0)
+	(*new_env) = malloc((*i + 1) * sizeof(char **));
+	if ((*new_env) == 0)
 		return (0);
-	*new_env[*i] = 0;
+	(*new_env)[*i] = 0;
 	while (*i-- > (pos + 1))
-		*new_env[*i] = envp[*i];
+		(*new_env)[*i] = envp[*i];
 	free(envp[*i]);
 	return (1);
 }
@@ -33,14 +33,14 @@ void	add_table(char ***new_env, char *add, int *i, char **envp)
 
 int	dup_add_table_short(char ***new_env, char *add, int *i, char **envp)
 {
-	*new_env = malloc((*i + 1 + 1) * sizeof(char *));
+	(*new_env) = malloc((*i + 1 + 1) * sizeof(char *));
 	if (new_env == 0)
 		return (0);
-	*new_env[*i + 1] = 0;
-	*new_env[*i] = ft_strdup(add);
+	(*new_env)[*i + 1] = 0;
+	(*new_env)[*i] = ft_strdup(add);
 	if (new_env[*i] == 0)
 		return (0);
 	while (*i-- > 0)
-		*new_env[*i] = envp[*i];
+		(*new_env)[*i] = envp[*i];
 	return (1);
 }
