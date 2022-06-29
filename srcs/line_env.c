38b69dbb1e7984237a4d33cfa5f615177010ac95
|°_°| <grivel.theo@protonmail.com>
2022-06-29 21:20:53 +0200

    WIP: Norminette line_env.c (#77)
    * tab to space
    
    * Removed ()
    
    * Included my_getenv
    
    * Some fixes of norm
    
    * Replaced variables of int by a int table
    
    * Split in a seconde function
    
    * Split in a sub-function single quote
    
    * Split in a sub-function dollars

--- srcs/line_env.c
+++ srcs/line_env.c
@@ -6,13 +6,13 @@
 /*   By: ldominiq <ldominiq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 16:21:56 by tgrivel           #+#    #+#             */
-/*   Updated: 2022/06/29 19:58:44 by ldominiq         ###   ########.fr       */
+/*   Updated: 2022/06/29 20:59:19 by melogr@phy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"minishell.h"
 
-//	an enviromnent variable [A-Z] or [a-z] or '_'
+// an enviromnent variable [A-Z] or [a-z] or '_'
 static int	name_env(char c)
 {
 	if (c == '_')
@@ -24,8 +24,8 @@ static int	name_env(char c)
 	return (0);
 }
 
-//	get length of the enviromnent variable
-static char	*get_envlen(char *line, int *i, int *len)
+// get length of the enviromnent variable
+static char	*get_envlen(char *line, char **envp, int *i, int *len)
 {
 	char	*env;
 	char	*var;
@@ -37,15 +37,15 @@ static char	*get_envlen(char *line, int *i, int *len)
 		(*i)++;
 	}
 	var = ft_strndup((line + start), *i - start);
-	env = getenv(var);
+	env = my_getenv(var, envp);
 	free(var);
 	if (len)
 		*len += ft_strlen(env);
 	return (env);
 }
 
-//	get length of the line if we replace all environment variable
-static int	get_len(char *line)
+// get length of the line if we replace all environment variable
+static int	get_len(char *line, char **envp)
 {
 	int		i;
 	int		len;
@@ -68,8 +68,9 @@ static int	get_len(char *line)
 			i++;
 		}
 		while (line[i] == '$' && ++i)
-			get_envlen(line, &i, &len);
-		while (line[i] && line[i] != '\"' && (line[i] != '\'' || !td) && line[i] != '$' && ++i)
+			get_envlen(line, envp, &i, &len);
+		while (line[i] && line[i] != '\"'
+			&& (line[i] != '\'' || !td) && line[i] != '$' && ++i)
 			len++;
 	}
 	if (td == 0)
@@ -77,54 +78,78 @@ static int	get_len(char *line)
 	return (len);
 }
 
-//	return new line where the environment variable are replaced
-char	*line_env(char *line, char **envp)
+// While single quote
+static inline void	simple_quote(char *ret, char *line, int ind[5])
 {
-	(void)envp;
-	int		i;
-	int		r;
-	int		e;
-	int		len;
-	char	*env;
-	char	*ret;
-	int		td;
+	ret[ind[1]++] = line[ind[0]++];
+	while (line[ind[0]] && line[ind[0]] != '\'')
+		ret[ind[1]++] = line[ind[0]++];
+	ret[ind[1]++] = line[ind[0]++];
+}
 
-	len = get_len(line);
-	if (len == -1)
+// While there is dollars in line
+static void	dollars(char *ret, char *line, int ind[5], char **envp)
+{
+	char	*env;
+
+	env = get_envlen(line, envp, &ind[0], 0);
+	ind[2] = 0;
+	while (env && env[ind[2]])
+		ret[ind[1]++] = env[ind[2]++];
+}
+
+// ind[0]: index of line
+// ind[1]: index of the return line
+// ind[2]: index of environment variable
+// ind[3]: toggle double quotes
+// ind[4]: lenght of the return string
+static char	*init_index(int ind[5], char *line, char **envp)
+{
+	char	*ret;
+
+	ind[0] = 0;
+	ind[1] = 0;
+	ind[2] = 0;
+	ind[3] = 1;
+	ind[4] = get_len(line, envp);
+	if (ind[4] == -1)
+	{
 		free(line);
-	if (len == -1)
 		return (0);
-	ret = malloc(len + 1);
+	}
+	ret = malloc(ind[4] + 1);
 	if (ret == 0)
 		return (0);
-	ret[len] = 0;
-	i = 0;
-	r = 0;
-	td = 1;
-	while ((line)[i])
+	return (ret);
+}
+
+// return new line where the environment variable are replaced
+// env: the string of environment variable
+// ret: the returned string
+// ind: the table of index, lenght and toggle variable
+char	*line_env(char *line, char **envp)
+{
+	char	*ret;
+	int		ind[5];
+
+	ret = init_index(ind, line, envp);
+	if (ret == 0)
+		return (0);
+	ret[ind[4]] = 0;
+	while (line[ind[0]])
 	{
-		if ((line)[i] == '\"')
+		if (line[ind[0]] == '\"')
 		{
-			ret[r++] = (line)[i++];
-			td = (td + 1) % 2;
+			ret[ind[1]++] = line[ind[0]++];
+			ind[3] = (ind[3] + 1) % 2;
 		}
-		while ((line)[i] == '\'' && td)
-		{
-			ret[r++] = (line)[i++];
-			while ((line)[i] && (line)[i] != '\'')
-				ret[r++] = (line)[i++];
-			ret[r++] = (line)[i++];
-		}
-		while ((line)[i] == '$' && ++i)
-		{
-			env = get_envlen((line), &i, 0);
-			e = 0;
-			while (env && env[e])
-				ret[r++] = env[e++];
-		}
-		while ((line)[i] && ((line)[i] != '\"' || td) &&
-			((line)[i] != '\'' || !td) && (line)[i] != '$')
-			ret[r++] = (line)[i++];
+		while (line[ind[0]] == '\'' && ind[3])
+			simple_quote(ret, line, ind);
+		while (line[ind[0]] == '$' && ++ind[0])
+			dollars(ret, line, ind, envp);
+		while (line[ind[0]] && (line[ind[0]] != '\"' || ind[3])
+			&& (line[ind[0]] != '\'' || !ind[3]) && line[ind[0]] != '$')
+			ret[ind[1]++] = line[ind[0]++];
 	}
 	free(line);
 	return (ret);
