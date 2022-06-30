653b0a05d091b8a74752c712a89d589aafafba64
Lucas Dominique <lucasdominique99@gmail.com>
2022-06-30 08:28:56 +0200

    Norminette friendly line_env.c

--- srcs/line_env.c
+++ srcs/line_env.c
@@ -6,83 +6,11 @@
 /*   By: ldominiq <ldominiq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 16:21:56 by tgrivel           #+#    #+#             */
-/*   Updated: 2022/06/29 21:31:03 by melogr@phy       ###   ########.fr       */
+/*   Updated: 2022/06/30 08:23:54 by ldominiq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"minishell.h"
-
-// an enviromnent variable [A-Z] or [a-z] or '_'
-static int	name_env(char c)
-{
-	if (c == '_')
-		return (1);
-	if ('a' <= c && c <= 'z')
-		return (1);
-	if ('A' <= c && c <= 'Z')
-		return (1);
-	return (0);
-}
-
-// get length of the enviromnent variable
-static char	*get_envlen(char *line, char **envp, int *i, int *len)
-{
-	char	*env;
-	char	*var;
-	int		start;
-
-	start = *i;
-	while (name_env(line[*i]))
-	{
-		(*i)++;
-	}
-	var = ft_strndup((line + start), *i - start);
-	env = my_getenv(var, envp);
-	free(var);
-	if (len)
-		*len += ft_strlen(env);
-	return (env);
-}
-
-// ind[0]: index of line
-// ind[1]: toggle double quote
-// ind[2]: length of the environment variable
-static inline void	init_index_get_len(int ind[3])
-{
-	ind[0] = 0;
-	ind[1] = 1;
-	ind[2] = 0;
-}
-
-// get length of the line if we replace all environment variable
-static int	get_len(char *line, char **envp)
-{
-	int	ind[3];
-
-	init_index_get_len(ind);
-	while (line[ind[0]])
-	{
-		if (line[ind[0]] == '\"' && ++ind[0] && ++ind[2])
-			ind[1] = (ind[1] + 1) % 2;
-		while (ind[1] && line[ind[0]] == '\'' && ++ind[0])
-		{
-			ind[2] += 2;
-			while (line[ind[0]] && line[ind[0]] != '\'' && ++ind[0])
-				ind[2]++;
-			if (line[ind[0]] == 0)
-				return (-1);
-			ind[0]++;
-		}
-		while (line[ind[0]] == '$' && ++ind[0])
-			get_envlen(line, envp, &(ind[0]), &(ind[2]));
-		while (line[ind[0]] && line[ind[0]] != '\"' && (line[ind[0]] != '\''
-				|| !ind[1]) && line[ind[0]] != '$' && ++ind[0])
-			ind[2]++;
-	}
-	if (ind[1] == 0)
-		return (-1);
-	return (ind[2]);
-}
 
 // While single quote
 static inline void	simple_quote(char *ret, char *line, int ind[5])
---
+++ srcs/utils_line_env.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_line_env.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldominiq <ldominiq@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/30 08:23:24 by ldominiq          #+#    #+#             */
+/*   Updated: 2022/06/30 08:26:11 by ldominiq         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./minishell.h"
+
+// an enviromnent variable [A-Z] or [a-z] or '_'
+static int	name_env(char c)
+{
+	if (c == '_')
+		return (1);
+	if ('a' <= c && c <= 'z')
+		return (1);
+	if ('A' <= c && c <= 'Z')
+		return (1);
+	return (0);
+}
+
+// get length of the enviromnent variable
+char	*get_envlen(char *line, char **envp, int *i, int *len)
+{
+	char	*env;
+	char	*var;
+	int		start;
+
+	start = *i;
+	while (name_env(line[*i]))
+	{
+		(*i)++;
+	}
+	var = ft_strndup((line + start), *i - start);
+	env = my_getenv(var, envp);
+	free(var);
+	if (len)
+		*len += ft_strlen(env);
+	return (env);
+}
+
+// ind[0]: index of line
+// ind[1]: toggle double quote
+// ind[2]: length of the environment variable
+static inline void	init_index_get_len(int ind[3])
+{
+	ind[0] = 0;
+	ind[1] = 1;
+	ind[2] = 0;
+}
+
+// get length of the line if we replace all environment variable
+int	get_len(char *line, char **envp)
+{
+	int	ind[3];
+
+	init_index_get_len(ind);
+	while (line[ind[0]])
+	{
+		if (line[ind[0]] == '\"' && ++ind[0] && ++ind[2])
+			ind[1] = (ind[1] + 1) % 2;
+		while (ind[1] && line[ind[0]] == '\'' && ++ind[0])
+		{
+			ind[2] += 2;
+			while (line[ind[0]] && line[ind[0]] != '\'' && ++ind[0])
+				ind[2]++;
+			if (line[ind[0]] == 0)
+				return (-1);
+			ind[0]++;
+		}
+		while (line[ind[0]] == '$' && ++ind[0])
+			get_envlen(line, envp, &(ind[0]), &(ind[2]));
+		while (line[ind[0]] && line[ind[0]] != '\"' && (line[ind[0]] != '\''
+				|| !ind[1]) && line[ind[0]] != '$' && ++ind[0])
+			ind[2]++;
+	}
+	if (ind[1] == 0)
+		return (-1);
+	return (ind[2]);
+}
